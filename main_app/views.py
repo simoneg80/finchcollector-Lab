e29bc4bc7e768df87a1f54c0d3ef034bdff55cc3
Simone Green <simoneg80@yahoo.com>
2023-11-25 16:21:10 -0500

    Create view all page and add css

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+# Temporary fake data until we connect to a database
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches': finches})

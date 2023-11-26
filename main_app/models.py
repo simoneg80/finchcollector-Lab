065f0a02bcabc9b752facdf513f528158e1c131b
Simone Green <simoneg80@yahoo.com>
2023-11-26 16:02:40 -0500

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  population = models.IntegerField()
+  
+  # Chaning this instance method to a string method
+  # This does not impact the database, therefor no migration is needed
+  def __str__(self):
+    return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-# Temporary fake data until we connect to a database
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
@@ -13,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  return render(request, 'finches/index.html', {'finches': finches})
+  # finches = Finch.objects.all()
+  return render(request, 'finches/index.html', )
